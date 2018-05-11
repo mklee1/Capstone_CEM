85e5ae56fc7f0019bd8fc286aba2a30e0a16006f
Mitchell Lee <mklee1@andrew.cmu.edu>
2018-05-11 03:52:37 -0400

    add driver for java

--- java/Driver.java
+++ java/Driver.java
@@ -1,34 +1,35 @@
-import java.io.File;
-import java.io.IOException;
-import java.awt.image.BufferedImage;
-import java.awt.Image;
-
-import javax.imageio.ImageIO;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Driver {
-	public static void main(String args[]) {
-		int[] bounds;
-		int[][] img;
-		int[][] seg;
-		ImageProcessor ip = new ImageProcessor();
-		try {
-            BufferedImage image = ImageIO.read(new File("test_segment2.jpg"));
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) {
+        ParamParser pp = new ParamParser();
+        try {
+        	pp.parse_params();
+        	System.out.println("hi");
+        	Process p = Runtime.getRuntime().exec("python condensed.py");
+        	BufferedReader stdInput = new BufferedReader(new 
+                    InputStreamReader(p.getInputStream()));
+
+               BufferedReader stdError = new BufferedReader(new 
+                    InputStreamReader(p.getErrorStream()));
+        	String s = null;
+        	System.out.println("Here is the standard output of the command:\n");
+        	String result = "";
+            while ((s = stdInput.readLine()) != null) {
+            	result = result + s.substring(1,2);
+            }
+            System.out.println(result);
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+        	System.out.println("bye");
         }
-		img = ip.ndarrayTo2DList(image);
-
-		bounds = ip.getBounds(img, 0, 1);
-		image = ip.cutImage(img, bounds); 
-		ip.print2DList(img);
-
-		bounds = ip.digitSegment(img,0);
-		while (bounds[0] != -1) {
-		    seg = ip.cutImage(img, bounds);
-		    ip.print2DList(seg);
-		    int limit = bounds[3]+1;
-		    bounds = ip.digitSegment(img, limit);
-		}
-	}
+        
+        catch (Exception e) {
+        	e.printStackTrace();
+        }
+        System.out.println("Finished parsing");
+    }
 }
-
